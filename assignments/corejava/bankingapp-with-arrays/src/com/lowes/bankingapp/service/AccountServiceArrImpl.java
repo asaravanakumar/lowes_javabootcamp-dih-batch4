d56eeeb0604d575283d0c19f9d0f175eb6de0ea4
Devops Training <saravana.devopstraining2023@gmail.com>
2024-07-12 07:46:04 +0530

    java labs

--- assignments/corejava/bankingapp-with-arrays/src/com/lowes/bankingapp/service/AccountServiceArrImpl.java
+++ assignments/corejava/bankingapp-with-arrays/src/com/lowes/bankingapp/service/AccountServiceArrImpl.java
@@ -1,16 +1,30 @@
 package com.lowes.bankingapp.service;
 
+import com.lowes.bankingapp.exception.AccountValidationException;
 import com.lowes.bankingapp.model.Account;
+import com.lowes.bankingapp.model.AccountType;
 
 public class AccountServiceArrImpl implements AccountService {
 
     private Account[] accounts = new Account[10];
 
+    int counter;
+
     @Override
-    public int create(Account account) {
+    public int create(Account account) throws AccountValidationException {
+
+        if(account == null) {
+            throw  new AccountValidationException();
+        }
         // TODO: Logic to add account
-        return 0;
+        account.setId(++counter);
+        accounts[counter-1] = account;
+        return account.getId();
     }
+
+//    private boolean validateAccount(Account account) {
+//        AccountType.valueOf(account.getType())
+//    }
 
     @Override
     public boolean update(int id, Account account) {
